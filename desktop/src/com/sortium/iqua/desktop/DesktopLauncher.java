882f0490770b8609ec18464f0f20777521792c77
Lloydangifer <vmanrique@netcourrier.com>
2015-05-05 10:55:04 +0200

    added base libgdx project

---
+++ desktop/src/com/sortium/iqua/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sortium.iqua.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sortium.iqua.IquaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IquaGame(), config);
+	}
+}
